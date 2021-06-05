248b64913b6bcd45e400be946afcfba87baec634
idanEngel <ida2423@gmail.com>
2021-06-05 20:03:19 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import "./App.css";
+import { useState, useRef } from "react";
+import OneDay from "./components/OneDay";
+import OneWeek from "./components/OneWeek";
+import OneMin from "./components/OneMin";
+import FiveMin from "./components/FiveMin";
 
 function App() {
+  const [component, setComponent] = useState("OneMinute");
+
+  const fiveMinutesRef = useRef();
+  const oneMinuteRef = useRef();
+  const oneDayRef = useRef();
+  const oneWeekRef = useRef();
+
+  const renderSwitch = (selctedComponent) => {
+    setComponent(selctedComponent);
+  };
+
+  const focus = (selctedTable) => {
+    switch (selctedTable) {
+      case "oneMinuteRef":
+        oneMinuteRef.current.className = "selected";
+        fiveMinutesRef.current.className = "";
+        oneDayRef.current.className = "";
+        oneWeekRef.current.className = "";
+        break;
+
+      case "fiveMinutesRef":
+        fiveMinutesRef.current.className = "selected";
+        oneMinuteRef.current.className = "";
+        oneDayRef.current.className = "";
+        oneWeekRef.current.className = "";
+        break;
+
+      case "oneDayRef":
+        oneDayRef.current.className = "selected";
+        fiveMinutesRef.current.className = "";
+        oneMinuteRef.current.className = "";
+        oneWeekRef.current.className = "";
+        break;
+
+      case "oneWeekRef":
+        oneWeekRef.current.className = "selected";
+        fiveMinutesRef.current.className = "";
+        oneDayRef.current.className = "";
+        oneMinuteRef.current.className = "";
+        break;
+
+      default:
+        break;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <table>
+          <tbody>
+            <tr style={{ padding: "1rem" }}>
+              <th
+                ref={fiveMinutesRef}
+                className="selected"
+                onClick={() => {
+                  renderSwitch("OneMinute");
+                  focus("fiveMinutesRef");
+                }}
+              >
+                One Minute
+              </th>
+              <th
+                ref={oneMinuteRef}
+                onClick={() => {
+                  renderSwitch("FiveMinutes");
+                  focus("oneMinuteRef");
+                }}
+              >
+                Five Minutes
+              </th>
+              <th
+                ref={oneDayRef}
+                onClick={() => {
+                  renderSwitch("OneDay");
+                  focus("oneDayRef");
+                }}
+              >
+                One Day
+              </th>
+              <th
+                ref={oneWeekRef}
+                onClick={() => {
+                  renderSwitch("OneWeek");
+                  focus("oneWeekRef");
+                }}
+              >
+                One Week
+              </th>
+            </tr>
+          </tbody>
+        </table>
+        {
+          {
+            OneDay: <OneDay />,
+            OneWeek: <OneWeek />,
+            OneMinute: <OneMin />,
+            FiveMinutes: <FiveMin />,
+          }[component]
+        }
+      </div>
+    </>
   );
 }
 
---
+++ src/components/OneMin.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  XAxis,
+  YAxis,
+  Area,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
+
+function OneMin() {
+  const [state, setstate] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.fxempire.com/api/v1/en/stocks/chart/candles?Identifier=AAPL.XNAS&IdentifierType=Symbol&AdjustmentMethod=All&IncludeExtended=False&period=1&Precision=Minutes&StartTime=8/28/2020%2016:0&EndTime=9/4/2020%2023:59&_fields=ChartBars.StartDate,ChartBars.High,ChartBars.Low,ChartBars.StartTime,ChartBars.Open,ChartBars.Close,ChartBars.Volume`
+      )
+      .then((res) => setstate(res.data));
+  }, []);
+
+  return (
+    <div className="App">
+      <ResponsiveContainer width="95%" height={400}>
+        <AreaChart data={state}>
+          <defs>
+            <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
+              <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+            </linearGradient>
+          </defs>
+          <Area dataKey="Close" stroke="#2451B7" fill="url(#color)" />
+          <XAxis dataKey="StartTime" axisLine="false" tickLine="false" />
+          <YAxis
+            datakey="Close"
+            tickCount="6"
+            domain={[
+              (dataMin) => Math.floor(dataMin),
+              (dataMax) => Math.ceil(dataMax),
+            ]}
+          />
+          <Tooltip content={<CustomTooltip />} />
+          <CartesianGrid opacity={0.1} />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+function CustomTooltip({ active, payload, label }) {
+  if (active && payload) {
+    return (
+      <div className="tooltip">
+        <h4>Date/Time: {label}</h4>
+        <p>Close: {payload[0].value.toFixed(2)} $</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default OneMin;
